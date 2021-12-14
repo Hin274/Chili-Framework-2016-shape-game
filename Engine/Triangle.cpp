950389a18caa2b32a468502ee9f3bb0fca4d5c28
Hin yu <Hin274@hotmail.com>
2021-12-14 16:16:24 +0000

    removed the double to get rid of the warnings

--- Engine/Triangle.cpp
+++ Engine/Triangle.cpp
@@ -45,24 +45,24 @@ void Triangle::Draw(Graphics& gfx) const
 	gfx.PutPixel(x_int + 9, y_int + 10, 255, 255, 0);
 	gfx.PutPixel(x_int + 10, y_int + 10, 255, 255, 0);
 	gfx.PutPixel(x_int + 5, y_int + 0, 255, 255, 0);
-	gfx.PutPixel(x_int + 4.5, y_int + 1, 255, 255, 0);
+	gfx.PutPixel(x_int + 4, y_int + 1, 255, 255, 0);
 	gfx.PutPixel(x_int + 4, y_int +	2 , 255, 255, 0);
-	gfx.PutPixel(x_int + 3.5, y_int + 3, 255, 255, 0);
+	gfx.PutPixel(x_int + 3, y_int + 3, 255, 255, 0);
 	gfx.PutPixel(x_int + 3, y_int + 4, 255, 255, 0);
-	gfx.PutPixel(x_int + 2.5, y_int + 5, 255, 255, 0);
+	gfx.PutPixel(x_int + 2, y_int + 5, 255, 255, 0);
 	gfx.PutPixel(x_int + 2, y_int + 6, 255, 255, 0);
-	gfx.PutPixel(x_int + 1.5, y_int + 7, 255, 255, 0);
+	gfx.PutPixel(x_int + 1, y_int + 7, 255, 255, 0);
 	gfx.PutPixel(x_int + 1, y_int + 8, 255, 255, 0);
-	gfx.PutPixel(x_int + 0.5, y_int + 9, 255, 255, 0);
-	gfx.PutPixel(x_int + 9.5, y_int + 9, 255, 255, 0);
+	gfx.PutPixel(x_int + 0, y_int + 9, 255, 255, 0);
+	gfx.PutPixel(x_int + 9, y_int + 9, 255, 255, 0);
 	gfx.PutPixel(x_int + 9, y_int + 8, 255, 255, 0);
-	gfx.PutPixel(x_int + 8.5, y_int + 7, 255, 255, 0);
+	gfx.PutPixel(x_int + 8, y_int + 7, 255, 255, 0);
 	gfx.PutPixel(x_int + 8, y_int + 6, 255, 255, 0);
-	gfx.PutPixel(x_int + 7.5, y_int + 5, 255, 255, 0);
+	gfx.PutPixel(x_int + 7, y_int + 5, 255, 255, 0);
 	gfx.PutPixel(x_int + 7, y_int + 4, 255, 255, 0);
-	gfx.PutPixel(x_int + 6.5, y_int + 3, 255, 255, 0);
+	gfx.PutPixel(x_int + 6, y_int + 3, 255, 255, 0);
 	gfx.PutPixel(x_int + 6, y_int + 2, 255, 255, 0);
-	gfx.PutPixel(x_int + 5.5, y_int + 1, 255, 255, 0);
+	gfx.PutPixel(x_int + 5, y_int + 1, 255, 255, 0);
 	
 }
 
