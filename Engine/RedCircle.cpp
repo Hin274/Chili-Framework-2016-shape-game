3cdbb9ba286037986e285f17e33b69d5fb87c11f
Hin yu <Hin274@hotmail.com>
2021-12-15 17:02:24 +0000

    Updated red circle with vec pos and vec vel

--- Engine/RedCircle.cpp
+++ Engine/RedCircle.cpp
@@ -1,18 +1,16 @@
 #include "RedCircle.h"
 
-void RedCircle::Init(float in_x, float in_y, float in_vx, float in_vy)
+void RedCircle::Init(const Vec2& pos_in, const Vec2& vel_in)
 {
-	x = in_x;
-	y = in_y;
-	vx = in_vx;
-	vy = in_vy;
+	pos = pos_in;
+	vel = vel_in;
 
 }
 
 void RedCircle::Draw(Graphics& gfx) const
 {
-	const int x_int = int(x);
-	const int y_int = int(y);
+	const int x_int = int(pos.x);
+	const int y_int = int(pos.y);
 
 		gfx.PutPixel(x_int + 3, y_int + 0, 255, 0, 0);
 		gfx.PutPixel(x_int + 4, y_int + 0, 255, 0, 0);
@@ -47,31 +45,31 @@ void RedCircle::Draw(Graphics& gfx) const
 
 void RedCircle::Movement(float dt)
 {
-	x += vx*dt;
-	y += vy*dt;
+	pos.x += vel.x*dt;
+	pos.y += vel.y*dt;
 
-	const float right = x + width;
-	if (x < 0)
+	const float right = pos.x + width;
+	if (pos.x < 0)
 	{
-		x = 0;
-		vx = -vx;
+		pos.x = 0;
+		vel.x = -vel.x;
 	}
 	else if (right >= float(Graphics::ScreenWidth))
 	{
-		x = float (Graphics::ScreenWidth - 1) - width;
-		vx = -vx;
+		pos.x = float (Graphics::ScreenWidth - 1) - width;
+		vel.x = -vel.x;
 	}
 
-	const float bottom = y + height;
-	if (y < 0)
+	const float bottom = pos.y + height;
+	if (pos.y < 0)
 	{
-		y = 0;
-		vy = -vy;
+		pos.y = 0;
+		vel.y = -vel.y;
 	}
 	else if (bottom >= float(Graphics::ScreenHeight))
 	{
-		y = float(Graphics::ScreenHeight - 1) - height;
-		vy = -vy;
+		pos.y = float(Graphics::ScreenHeight - 1) - height;
+		vel.y = -vel.y;
 	}
 
 }
@@ -80,13 +78,13 @@ void RedCircle::collection(const Circle&circle)
 {
 	const float circleright = circle.GetPos().x + circle.GetWidth();
 	const float circlebottom = circle.GetPos().y + circle.GetHeight();
-	const float redcircleright = x + width;
-	const float redcirclebottom = y + height;
+	const float redcircleright = pos.x + width;
+	const float redcirclebottom = pos.y + height;
 
 	if (
-		circleright >= x &&
+		circleright >= pos.x &&
 		circle.GetPos().x <= redcircleright &&
-		circlebottom >= y &&
+		circlebottom >= pos.y &&
 		circle.GetPos().y <= redcirclebottom)
 	{
 		collected = true;
@@ -98,13 +96,13 @@ bool RedCircle::collect(const Circle& circle)
 {
 	const float circleright = circle.GetPos().x + circle.GetWidth();
 	const float circlebottom = circle.GetPos().y + circle.GetHeight();
-	const float redcircleright = x + width;
-	const float redcirclebottom = y + height;
+	const float redcircleright = pos.x + width;
+	const float redcirclebottom = pos.y + height;
 
 	return
-		circleright >= x &&
+		circleright >= pos.x &&
 		circle.GetPos().x <= redcircleright &&
-		circlebottom >= y &&
+		circlebottom >= pos.y &&
 		circle.GetPos().y <= redcirclebottom;
 }
 
--- Engine/RedCircle.h
+++ Engine/RedCircle.h
@@ -1,22 +1,21 @@
 #pragma once
 #include "Graphics.h"
 #include "Circle.h"
+#include "Vec2.h"
 
 
 class RedCircle
 {
 public:
-	void Init(float in_x, float in_y, float in_vx, float in_vy);
+	void Init(const Vec2& pos_in, const Vec2& vel_in);
 	void Draw(Graphics& gfx) const;
 	void Movement(float dt);
 	void collection(const Circle& circle);
 	bool collect(const Circle& circle);
 	bool Collected() const;
 private:
-	float x = 0.0f;
-	float y = 0.0f;
-	float vx = 0.0f;
-	float vy = 0.0f;
+	Vec2 pos;
+	Vec2 vel;
 	static constexpr float width = 10.0f;
 	static constexpr float height = 10.0f;
 	bool collected = false;
